8b6be399e1465d13aff64a795811960acd1e10b2
jainil <jainiljain.dev@gmail.com>
2025-06-30 00:13:00 +0530

    add cal and footer

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-Shery.mouseFollower();
-Shery.makeMagnet(".magnet");
+  Shery.mouseFollower();
+  Shery.makeMagnet(".magnet");
 
-window.addEventListener("wheel", function(dets){
-    if(dets.deltaY>0){
-        gsap.to(".marquee",{
-        transform:'translateX(-200%)',
-        duration:4,
-        repeat:-1,
-        ease:"none",
-    })
-     gsap.to(".marquee img",{
-        rotate:180
-        })
-    }else{
-         gsap.to(".marquee",{
-        transform:'translateX(0%)',
-        duration:4,
-        repeat:-1,
-        ease:"none",
-        })
-        gsap.to(".marquee img",{
-        rotate:0
-        })
-    }
-})
+    window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+    const content = document.getElementById("mainContent");
+
+    // Step 1: Slide loader up
+    loader.classList.add("slide-up");
+
+    // Step 2: After transition (1s), hide loader and show content
+    setTimeout(() => {
+      loader.style.display = "none";
+      content.classList.remove("opacity-0");
+      content.classList.add("opacity-100", "transition-opacity", "duration-1000");
+    }, 1000); // Match with transition duration
+  });
+
+  window.addEventListener("wheel", function(dets){
+      if(dets.deltaY>0){
+          gsap.to(".marquee",{
+          transform:'translateX(-200%)',
+          duration:4,
+          repeat:-1,
+          ease:"none",
+      })
+      gsap.to(".marquee img",{
+          rotate:180
+          })
+      }else{
+          gsap.to(".marquee",{
+          transform:'translateX(0%)',
+          duration:4,
+          repeat:-1,
+          ease:"none",
+          })
+          gsap.to(".marquee img",{
+          rotate:0
+          })
+      }
+  })
 
 
-gsap.to(".fleftelm",{
+  gsap.to(".fleftelm",{
     scrollTrigger:{
         trigger: '.fimages',
         pin:true,
@@ -58,7 +73,7 @@ Shery.imageEffect(".images", {
 });
 
   ScrollTrigger.create({
-      trigger: ".images",
+      trigger: ".featured",
       start: "top center",
       end: "bottom center",
       onEnter: () => gsap.to("body", { backgroundColor: "#000", duration: 0.5 }),
@@ -66,47 +81,41 @@ Shery.imageEffect(".images", {
     
 );
 
-// ScrollTrigger.create({
-//   trigger: ".projects",
-//   start: "top top",         // When .projects hits the top of the viewport
-//   end: "bottom bottom",     // Until the bottom of .projects hits bottom of viewport
-//   onEnter: () => {
-//     gsap.to("body", { backgroundColor: "#fff", duration: 0.5, overwrite: 'auto' });
-//   },
-//   onEnterBack: () => {
-//     gsap.to("body", { backgroundColor: "#fff", duration: 0.5, overwrite: 'auto' });
-//   },
-//   onLeave: () => {
-//     gsap.to("body", { backgroundColor: "#000", duration: 0.5, overwrite: 'auto' });
-//   },
-//   onLeaveBack: () => {
-//     gsap.to("body", { backgroundColor: "#000", duration: 0.5, overwrite: 'auto' });
-//   },
-//   // markers: true // Uncomment for debugging
-// });
+  const sliderWrapper = document.querySelector(".slider-wrapper");
+  const totalWidth = sliderWrapper.scrollWidth;
 
-
-// Horizontal Scroll for Projects
-const sliderWrapper = document.querySelector(".slider-wrapper");
-const totalWidth = sliderWrapper.scrollWidth;
-
-gsap.to(sliderWrapper, {
-  x: () => -(totalWidth - window.innerWidth + 300), // 300 = padding
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".projects",
-    start: "top top",
-    end: () => "+=" + (totalWidth - window.innerWidth + 300),
-    scrub: true,
-    pin: true,
-    anticipatePin: 1,
-    onUpdate: self => {
-      const progress = self.progress;
-      if (progress > 0 && progress < 1) {
-        gsap.to("body", { backgroundColor: "#fff", duration: 0.3, overwrite: true });
-      } else {
-        gsap.to("body", { backgroundColor: "#000", duration: 0.3, overwrite: true });
-      }
+  gsap.to(sliderWrapper, {
+    x: () => -(totalWidth - window.innerWidth + 300), // 300 = padding
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".projects",
+      start: "top top",
+      end: () => "+=" + (totalWidth - window.innerWidth + 300),
+      scrub: true,
+      pin: true,
+      anticipatePin: 1,
+      onUpdate: self => {
+        const progress = self.progress;
+        if (progress > 0 && progress < 1) {
+          gsap.to("body", { backgroundColor: "#fff", duration: 0.3, overwrite: true });
+        } else {
+          gsap.to("body", { backgroundColor: "#000", duration: 0.3, overwrite: true });
+        }
+      },
     },
-  },
-});
+  });
+
+      
+  gsap.utils.toArray(".testimonial-card").forEach((card, index) => {
+        gsap.from(card, {
+          opacity: 0,
+          y: 50,
+          duration: 0.8,
+          delay: index * 0.01,
+          scrollTrigger: {
+            trigger: card,
+            start: "top 90%",
+            toggleActions: "play none none reverse"
+          }
+        });
+      });
